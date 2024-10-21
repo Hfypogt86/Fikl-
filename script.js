45adfbb8bb225619a46b907be9b6d9c8d6e64900
Hfypogt86 <112763983+Hfypogt86@users.noreply.github.com>
2024-10-22 00:40:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function encryptMessage() {
     try {
         let message = document.getElementById('inputMessage').value;
-        let encrypted = btoa(message);
+        let encrypted = Base64.encode(message);
         document.getElementById('outputMessage').value = encrypted;
     } catch (error) {
         document.getElementById('outputMessage').value = "خطأ في التشفير: " + error.message;
@@ -11,7 +11,7 @@ function encryptMessage() {
 function decryptMessage() {
     try {
         let encrypted = document.getElementById('outputMessage').value;
-        let decrypted = atob(encrypted);
+        let decrypted = Base64.decode(encrypted);
         document.getElementById('inputMessage').value = decrypted;
     } catch (error) {
         document.getElementById('inputMessage').value = "خطأ في فك التشفير: " + error.message;
